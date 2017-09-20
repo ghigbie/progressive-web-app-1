9b13bf0e5e2540b1bbd9225db019258ce620b5d0
George Higbie <gehigbie@gmail.com>
2017-09-20 14:41:31 +0000

    Add service worker registration

---
+++ public/src/js/app.js
@@ -0,0 +1,9 @@
+/* global navigator*/
+
+if('serviceWorker' in navigator){
+    navigator.serviceWorker
+        .register('/sw.js')
+        .then(() => {
+            console.log('Service worker registered! Yay!!!');
+        });
+}
