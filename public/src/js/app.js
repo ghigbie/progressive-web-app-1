33f39e308b92b40ff855434ebdcf084336c07c3d
George Higbie <gehigbie@gmail.com>
2017-09-22 13:58:15 +0000

    Add fetch event

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -2,7 +2,7 @@
 
 if('serviceWorker' in navigator){ //this checks the browser for a service worker
     navigator.serviceWorker
-        .register('/sw.js')
+        .register('/sw.js') //you can add an additional object as a second argument to restrict the scope of the service worker
         .then(() => {
             console.log('Service worker registered! Yay!!!');
         });
