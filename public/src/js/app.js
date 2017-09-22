a47ebcbf223c1148bbd9bf8095f1174c649fd665
George Higbie <gehigbie@gmail.com>
2017-09-22 13:43:50 +0000

    Add comment to explain service worker checking

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,6 +1,6 @@
 /* global navigator*/
 
-if('serviceWorker' in navigator){
+if('serviceWorker' in navigator){ //this checks the browser for a service worker
     navigator.serviceWorker
         .register('/sw.js')
         .then(() => {
