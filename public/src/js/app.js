23be87f46ad01b1cc55c53f6daaf3993fb82dd7d
George Higbie <gehigbie@gmail.com>
2017-09-25 14:05:05 +0000

    Modify service worker

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,6 @@
 /* global navigator*/
+var deferredPrompt;
+
 
 if('serviceWorker' in navigator){ //this checks the browser for a service worker
     navigator.serviceWorker
@@ -7,3 +9,11 @@ if('serviceWorker' in navigator){ //this checks the browser for a service worker
             console.log('Service worker registered! Yay!!!');
         });
 }
+
+
+window.addEventListener('beforeinstallprompt', function(event){
+   console.log('beforeinstallprompt fired');
+   event.preventDefault();
+   deferredPrompt = event;
+   return false;
+});
