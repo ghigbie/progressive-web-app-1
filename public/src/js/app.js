13e733f5e4884f9ca3b790ba52a14bac021d87b5
George Higbie <gehigbie@gmail.com>
2017-09-27 13:53:59 +0000

    Add promise with catch block

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,7 +1,6 @@
 /* global navigator*/
 var deferredPrompt;
 
-
 if('serviceWorker' in navigator){ //this checks the browser for a service worker
     navigator.serviceWorker
         .register('/sw.js') //you can add an additional object as a second argument to restrict the scope of the service worker
@@ -16,4 +15,19 @@ window.addEventListener('beforeinstallprompt', function(event){
    event.preventDefault();
    deferredPrompt = event;
    return false;
+});
+
+let promise = new Promise(function(resolve, reject){
+    setTimeout(function() {
+       // resolve('This is from within setTimeout');
+       reject({code: 500, message: 'Something went wrong!'})
+    }, 3000);
+});
+
+promise.then(function(text){
+    return text;
+}).then(function(newText){
+    console.log(newText)
+}).catch(function(err){
+    console.log(err.code, err.message);
 });
